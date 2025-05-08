13075fefb7b0fd61505237c4a4ce62b73006d7df
Franciscoffv <franffv0809@gmail.com>
2025-05-07 19:00:22 -0600

    2

---
+++ VeterinariaEP/src/main/java/Veterinaria/vete/controller/CategoriaController.java
@@ -0,0 +1,63 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+
+package Veterinaria.vete.controller;
+
+
+
+import Veterinaria.vete.domain.Categoria;
+import Veterinaria.vete.service.CategoriaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/categoria")
+public class CategoriaController {
+  
+
+    @Autowired
+    private CategoriaService categoriaService;
+    
+    @GetMapping("/listado")
+    private String listado(Model model) {
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);        
+        model.addAttribute("totalCategorias",categorias.size());
+        return "/categoria/listado";
+    }
+    
+     @GetMapping("/nuevo")
+    public String categoriaNuevo(Categoria categoria) {
+        return "/categoria/modifica";
+    }
+
+    @PostMapping("/guardar")
+    public String categoriaGuardar(Categoria categoria) {        
+        categoriaService.save(categoria);
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/eliminar/{idCategoria}")
+    public String categoriaEliminar(Categoria categoria) {
+        categoriaService.delete(categoria);
+        return "redirect:/categoria/listado";
+    }
+
+    @GetMapping("/modificar/{idCategoria}")
+    public String categoriaModificar(Categoria categoria, Model model) {
+        categoria = categoriaService.getCategoria(categoria);
+        model.addAttribute("categoria", categoria);
+        
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("categorias", categorias);
+        
+        return "/categoria/modifica";
+    }   
+}
